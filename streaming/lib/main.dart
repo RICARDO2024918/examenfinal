9c7afe6200d802d1096df16122e6f1d332619a05
Manuel1652 <124534814+Manuel1652@users.noreply.github.com>
2024-11-24 20:27:09 -0500

    nuevo1
    hola

--- streaming/lib/main.dart
+++ streaming/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:streaming/administrador_secion.dart';
 import 'package:streaming/inicio.dart';
 
 void main() {
@@ -15,4 +16,31 @@ class MyApp extends StatelessWidget {
       home: HomePage(),
     );
   }
+}
+
+const darkBlueColor = Color(0xff486579); //App theme
+
+class MyApp1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Enter Person details - CURD',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // primaryColor: Colors.orange,
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Enter Person details - CURD'),
+    );
+  }
+}
+
+//Add home page
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required String title}) : super(key: key);
+
+  String get title => "Enter Person details - CURD";
+
+  @override
+  State<MyHomePage> createState() => AdministradorSecion();
 }
