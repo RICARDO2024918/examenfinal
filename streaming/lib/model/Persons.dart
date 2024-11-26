725bcf7cf9b34f9edccf64fe8c54d38e6d0ab1dc
Pedro Valverde <77286190+Eduardo199614@users.noreply.github.com>
2024-11-26 07:42:37 -0500

    Cambio en personas

--- streaming/lib/model/Persons.dart
+++ streaming/lib/model/Persons.dart
@@ -1,27 +1,27 @@
-
 ///created the Modal Class of Person and their respective functions as shown below
 class Person {
+  final String? id;
+  final String? usuario;
+  final String? rol;
+  final String? address;
 
-   final String? id;
-   final String? name;
-   final String? age;
-   final String? address;
-
-   const Person({ this.id,  this.name, this.age, this.address});
+  const Person({this.id, this.usuario, this.rol, this.address});
 
   //Alternate way to use model by const.
   //const Person({ this.id,  this.name, this.age, this.address,  this.description});
 
-  factory Person.fromMap(Map<String, dynamic> json) =>
-      Person(id: json["id"], name: json["name"], age: json["age"], address: json["address"]);
+  factory Person.fromMap(Map<String, dynamic> json) => Person(
+      id: json["id"],
+      usuario: json["usuario"],
+      rol: json["rol"],
+      address: json["address"]);
 
   Map<String, dynamic> toMap() {
     return {
       'id': id,
-      'name': name,
-      'age': age,
+      'usuario': usuario,
+      'rol': rol,
       'address': address,
     };
   }
 }
-
